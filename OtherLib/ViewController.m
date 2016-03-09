df54d4f49bcc77dc97c20a42170079d6b9dcb7c0
wangpeng <694472907@qq.com>
2016-03-09 14:43:54 +0800

    本地音频

--- OtherLib/ViewController.m
+++ OtherLib/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "FSAudioStream.h"
+
 
 @interface ViewController ()
+
+@property(nonatomic,strong)FSAudioStream *audioStream;
+
 
 @end
 
@@ -16,12 +21,46 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    [self.audioStream play];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+-(NSURL *)getURL{
+//NSString *str =@"http://www.300.la/filestores/2016/02/20/692de68936d75eea6da4a75f87f3ab2f.mp3";
+    NSString *str =@"http://www.300.la/filestores/2014/08/20/93fde37c2aeae22c61a9c7b70b247f92.mp3";
+    
+    
+    NSURL *url =[NSURL URLWithString:str];
+
+    return url;
 }
+
+
+-(FSAudioStream *)audioStream{
+
+    if (!_audioStream) {
+        _audioStream =[[FSAudioStream alloc]initWithUrl:[self getURL]];
+        
+        _audioStream.onFailure =^(FSAudioStreamError error,NSString *str){
+        
+            NSLog(@"%@",str);
+        
+        };
+        
+        _audioStream.onCompletion =^(){
+        
+            NSLog(@"播放完毕");
+        
+        };
+        
+        [_audioStream setVolume:0.7];
+    }
+    return _audioStream;
+}
+
+
 
 @end
